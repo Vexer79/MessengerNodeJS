2e923858f8f664c0bb0e3d0c246096c8bf0e8b78
Vexer79 <rokytskyi.i@gmail.com>
2024-05-17 18:42:57 +0200

    fix: bug with height of menu

--- public/js/mobile.js
+++ public/js/mobile.js
@@ -5,4 +5,37 @@
         button.addEventListener("click", (event) => {
             menu && menu.classList.toggle("_active");
         });
+
+    const input = document.getElementById("inputField");
+
+    let touchstartX = 0;
+    let touchendX = 0;
+    let touchstartY = 0;
+    let touchendY = 0;
+
+    function checkDirection() {
+        const deltaX = touchendX - touchstartX;
+        const deltaY = touchendY - touchstartY;
+
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            if (deltaX > 0) {
+                menu.style.left = "0px";
+            } else {
+                menu.style.left = "-650px";
+                input.focus();
+            }
+        }
+    }
+
+    document.addEventListener("touchstart", (e) => {
+        touchstartX = e.changedTouches[0].screenX;
+        touchstartY = e.changedTouches[0].screenY;
+    });
+
+    document.addEventListener("touchend", (e) => {
+        touchendX = e.changedTouches[0].screenX;
+        touchendY = e.changedTouches[0].screenY;
+        checkDirection();
+    });
+
 })();
